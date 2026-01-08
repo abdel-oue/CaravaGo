9cbb5101d1d32986111912a1048b71a9f4c9fd0e
Noaman-id <noemanidelmou4@gmail.com>
2026-01-08 01:33:32 +0100

    init

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.routes.js';
-import externalRoutes from './routes/external.routes.js';
-
 dotenv.config({ path: '../.env' });
 
 const app = express();
