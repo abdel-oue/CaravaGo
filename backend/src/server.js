0355a9842b2720f84f3927131ef25a201a244423
abdel-oue <dryalapah@gmail.com>
2026-01-09 03:21:55 +0100

    Fixed conflicts

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,7 +45,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/locations', locationRoutes);
-
+app.use
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
